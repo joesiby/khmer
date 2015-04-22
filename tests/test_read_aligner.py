1011c647505b2944314f8edbedd345a44b513d3c
Luiz Irber <luiz.irber@gmail.com>
2015-04-22 15:04:29 -0400

    Use Record instead of _screed_record_dict; remove unused import

--- tests/test_read_aligner.py
+++ tests/test_read_aligner.py
@@ -1,10 +1,10 @@
-from __future__ import print_function
-from builtins import range
 #
 # This file is part of khmer, http://github.com/ged-lab/khmer/, and is
 # Copyright (C) Michigan State University, 2009-2013. It is licensed under
 # the three-clause BSD license; see doc/LICENSE.txt. Contact: ctb@msu.edu
 #
+from __future__ import print_function
+
 import khmer
 from nose.tools import assert_almost_equals
 
